3773dc363f6055b829cbb8879988955d15121945
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-11-30 23:43:09 -0600

    commit

---
+++ practice_projects/banking-program.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+    double balance;
+    int choice;
+
+    // Allows the program to keep asking the question until you say to stop
+    
+    do{
+        // Makes everything look cleaner
+        cout << "******************\n";
+        cout << "Enter your choice\n";
+        cout << "******************\n";
+        cout << "1. Show Balance\n";
+        cout << "2. Deposit Money\n";
+        cout << "3. Withdraw Money\n";
+        cout << "4. Exit\n\n";
+        cout << "What is your choice? ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+                showBalance(balance);
+                break;
+            case 2:
+                balance += deposit();
+                showBalance(balance);
+                break;
+            case 3:
+                balance -= withdraw(balance);
+                showBalance(balance);
+                break;
+            case 4:
+                cout << "Thanks for visiting\n";
+                break;
+            default:
+                cout << "Invalid response\n";
+        }
+    }while(choice != 4);
+
+    return 0;
+}
+
+void showBalance(double balance){
+    cout << "Your balance is $" << setprecision(2) << std::fixed << balance << '\n';
+}
+double deposit(){
+    double amount = 0;
+
+    cout << "How much would you like to deposit? ";
+    cin >> amount;
+
+    if(amount >  0){
+        return amount;
+    }
+
+    else{
+        cout << "That's not a valid amount\n";
+        return 0;
+    }
+}   
+double withdraw(double balance){
+    double amount = 0;
+
+    cout << "Enter amount to withdraw ";
+    cin >> amount;
+
+    if(amount > balance){
+        cout << "insufficient funds\n";
+        return 0;
+    }
+
+    else if(amount < 0){
+        cout << "That's not a valid amount\n";
+        return 0;
+    }
+
+    else{
+        return amount;
+    }
+}
