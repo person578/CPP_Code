02b408cbd41151a2c0ed9ea29374890b4df2eebf
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 23:21:41 -0600

    commit

--- sizeof.cpp
+++ sizeof.cpp
@@ -8,11 +8,11 @@ int main(){
     bool adult = true;
     char grades[] = {'A', 'B', 'C', 'D', 'F'};
 
-    cout << sizeof(gpa) << " bytes\n";
-    cout << sizeof(name) << " bytes\n";
-    cout << sizeof(grade) << " bytes\n";
-    cout << sizeof(adult) << " bytes\n";
-    cout << sizeof(grades) << " bytes\n";
+    cout << sizeof(gpa) << " byte(s)\n";
+    cout << sizeof(name) << " byte(s)\n";
+    cout << sizeof(grade) << " byte(s)\n";
+    cout << sizeof(adult) << " byte(s)\n";
+    cout << sizeof(grades) << " byte(s)\n";
 
     return 0;
 }
