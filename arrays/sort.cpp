42319307a3dce064949de5e864a7e7290fd8d9f9
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 13:42:29 -0600

    added the sort function in the main function to sort the array

--- arrays/sort.cpp
+++ arrays/sort.cpp
@@ -7,6 +7,8 @@ int main(){
     int array[] = {10, 15, 4, 6, 12, 50, 16, 81, 3, 2};
     int size = sizeof(array)/sizeof(array[0]);
 
+    sort(array, size);
+
     for(int element : array){
         cout << element << " ";
     }
@@ -16,5 +18,5 @@ int main(){
 }
 
 void sort(int array[], int size){
-    
+
 }
