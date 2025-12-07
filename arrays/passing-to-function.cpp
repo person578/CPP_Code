d44cd5b845cefe0e98d330a30a9a499f4a9c8ef5
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 12:23:35 -0600

    functions and the array and total added an array of prices and another variable for total  which uses a 'getTotal' function which I declared and added a base for

--- arrays/passing-to-function.cpp
+++ arrays/passing-to-function.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+double getTotal();
+
 int main(){
-    
+    double prices[] = {99.99, 50.00, 49.99, 12.99, 14.99};
+    double total = getTotal();
+
+    return 0;
+}
+
+double getTotal(){
 
     return 0;
 }
