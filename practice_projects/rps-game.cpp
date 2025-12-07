951c508f0b88ec23f85023c5ea5a7bd712d1a11d
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 21:55:26 -0600

    added the areas for what the functions do

--- practice_projects/rps-game.cpp
+++ practice_projects/rps-game.cpp
@@ -12,3 +12,16 @@ int main()
 
     return 0;
 }
+
+char getUserChoice(){
+
+}
+char getComputerChoice(){
+
+}
+void showChoice(char choice){
+
+}
+void chooseWinner(char player, char computer){
+    
+}
