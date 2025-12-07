40624a331f0c6d67b35c053bf4bf70c9e92be3cb
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 12:49:26 -0600

    added an array of numbers 1-10 and the rest of the variables I'll need and input for the user

--- arrays/search.cpp
+++ arrays/search.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(numbers)/sizeof(numbers[0]);
+    int index;
+    int myNum;
+
+    cout << "Enter element to search for (1-10): ";
+    cin >> myNum;
+
+
 
     return 0;
 }
