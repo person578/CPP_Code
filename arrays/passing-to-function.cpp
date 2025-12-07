0169181a6382dfa5a2718e06784bb5ecde02b8ba
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 12:33:18 -0600

    finished the getTotal function and added added extra things in the main function for it to calculate prices

--- arrays/passing-to-function.cpp
+++ arrays/passing-to-function.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-double getTotal();
+double getTotal(double prices[], int size);
 
 int main(){
     double prices[] = {99.99, 50.00, 49.99, 12.99, 14.99};
-    double total = getTotal();
+    int size = sizeof(prices)/sizeof(prices[0]);
+    double total = getTotal(prices, size);
+
+    cout << "$" << total << '\n';
 
     return 0;
 }
 
-double getTotal(){
+double getTotal(double prices[], int size){
+    double total = 0;
 
-    return 0;
+    for(int i = 0; i < size; i++){
+        total += prices[i];
+    }
+
+    return total;
 }
