7b7a8dcb3571bbb67ba68c27842c883b02b3726b
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 23:43:43 -0600

    actually iterating added an array of students and made it iterate for the size of the array

--- arrays/iterate.cpp
+++ arrays/iterate.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    string students[] = {"Ashdon", "Emily", "Jace", "James", "Kate", "Zach", "Zane"};
+
+    for(int i = 0; i < sizeof(students)/sizeof(string); i++){
+        cout << students[i] << '\n';
+    }
 
     return 0;
 }
