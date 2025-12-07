62c6f342f3ce6525421a191d27764e97746481c2
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 13:41:11 -0600

    added a sort function

--- arrays/sort.cpp
+++ arrays/sort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+void sort(int array[], int size);
+
 int main(){
     int array[] = {10, 15, 4, 6, 12, 50, 16, 81, 3, 2};
     int size = sizeof(array)/sizeof(array[0]);
@@ -11,4 +13,8 @@ int main(){
     cout << '\n';
     
     return 0;
+}
+
+void sort(int array[], int size){
+    
 }
