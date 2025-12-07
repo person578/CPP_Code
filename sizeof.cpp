4c2bbeb50faf2446ab11975c7508c5a8c22f377d
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 23:25:40 -0600

    changed the last cout for the grades array to find the amount of elements in the array. I also added an 'E' to the grades array

--- sizeof.cpp
+++ sizeof.cpp
@@ -6,13 +6,13 @@ int main(){
     double gpa = 2.5;
     char grade = 'F';
     bool adult = true;
-    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    char grades[] = {'A', 'B', 'C', 'D', 'E', 'F'};
 
     cout << sizeof(gpa) << " byte(s)\n";
     cout << sizeof(name) << " byte(s)\n";
     cout << sizeof(grade) << " byte(s)\n";
     cout << sizeof(adult) << " byte(s)\n";
-    cout << sizeof(grades) << " byte(s)\n";
+    cout << sizeof(grades)/sizeof(char) << " elements\n";
 
     return 0;
 }
