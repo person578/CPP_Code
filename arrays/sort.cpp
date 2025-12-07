f25944a8946950ca84de28d8ca3ccc51b533c212
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 13:39:46 -0600

    made a variable for the size of the array and made a foreach loop that shows the array unsorted

--- arrays/sort.cpp
+++ arrays/sort.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main(){
     int array[] = {10, 15, 4, 6, 12, 50, 16, 81, 3, 2};
-    
+    int size = sizeof(array)/sizeof(array[0]);
+
+    for(int element : array){
+        cout << element << " ";
+    }
+    cout << '\n';
     
     return 0;
 }
