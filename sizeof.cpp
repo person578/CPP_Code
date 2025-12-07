904424c6afb62c9137f9d7b8d158ba2b22f4948e
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 23:20:46 -0600

    added different variables and different cout's for the variables

--- sizeof.cpp
+++ sizeof.cpp
@@ -4,8 +4,15 @@ using namespace std;
 int main(){
     string name = "Ethan";
     double gpa = 2.5;
+    char grade = 'F';
+    bool adult = true;
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
 
+    cout << sizeof(gpa) << " bytes\n";
     cout << sizeof(name) << " bytes\n";
+    cout << sizeof(grade) << " bytes\n";
+    cout << sizeof(adult) << " bytes\n";
+    cout << sizeof(grades) << " bytes\n";
 
     return 0;
 }
