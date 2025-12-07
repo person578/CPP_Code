22aba644094d998943d0d0922ff4dc39df6f19ff
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 13:48:55 -0600

    finished the sort function so that it will fully sort the array and then print the sorted array

--- arrays/sort.cpp
+++ arrays/sort.cpp
@@ -18,5 +18,15 @@ int main(){
 }
 
 void sort(int array[], int size){
+    int temp;
 
+    for(int i = 0; i < size - 1; i++){
+        for(int j = 0; j < size - i - 1; j++){
+            if(array[j] > array[j + 1]){
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
 }
