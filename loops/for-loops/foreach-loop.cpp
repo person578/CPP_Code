e54dfd97a36414f0004282c630c5d281562ce149
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-07 11:55:42 -0600

    used the array that was in the 'iterate.cpp' file but used a foreach loop for it

--- loops/for-loops/foreach-loop.cpp
+++ loops/for-loops/foreach-loop.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-    
+    string students[] = {"Ashdon", "Emily", "Jace", "James", "Kate", "Zach", "Zane"};
+
+    for(std::string student : students){
+        cout << student << '\n';
+    }
 
     return 0;
 }
