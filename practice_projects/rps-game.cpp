42e6cba5c7e0c24987ff81986c0db03b42ccb690
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-06 21:52:23 -0600

    declared the functions for the program

--- practice_projects/rps-game.cpp
+++ practice_projects/rps-game.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-
+char getUserChoice();
+char getComputerChoice();
+void showChoice(char choice);
+void chooseWinner(char player, char computer);
 
 int main()
 {
-
+    
 
     return 0;
 }
