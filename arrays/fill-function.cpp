810482f98af9263351711ef7b4f72f864e0db96a
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-12 13:57:42 -0600

    made the SIZE var 99 instead of 100 so I could have the first third have pizza the 2 third have hamburgers and the last have hotdogs. This is also what I did with other fill functions

--- arrays/fill-function.cpp
+++ arrays/fill-function.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main(){
-    const int SIZE = 100;
+    const int SIZE = 99;
     string foods[SIZE];
 
-    fill(foods, foods + SIZE, "pizza");
+    fill(foods, foods + (SIZE/3), "Pizza");
+    fill(foods + (SIZE/3), foods + 2*(SIZE/3), "Hamburgers");
+    fill(foods + (SIZE/3)*2, foods + SIZE, "Hotdogs");
 
     for(string food : foods){
         cout << food << '\n';
