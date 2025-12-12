aef661923a1b0187c967a01611151116deef1544
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-12 12:36:53 -0600

    made a file to learn the 'fill' function that has an array (called foods) of strings with just 10 strings of pizza in it

---
+++ arrays/fill-function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string foods[10] = {"pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza"};
+
+    
+
+    return 0;
+}
