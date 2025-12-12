3559e57eec02786b82ce17a32e8607740d4c2933
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-12 12:44:45 -0600

    made the array have a size of 100 and made a fill that fills the entire array with the string 'pizza'

--- arrays/fill-function.cpp
+++ arrays/fill-function.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    string foods[10] = {"pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza", "pizza"};
+    string foods[100];
 
-    
+    fill(foods, foods + 100, "pizza");
 
     for(string food : foods){
         cout << food << '\n';
