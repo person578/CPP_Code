bf25864db543b22ffb8733f47487c3fee021cec3
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-13 14:35:15 -0600

    get input and put in array I made a string array called foods with a size of 5 and a size variable for the size of the array also made for loop that has you enter a food you like then puts that in a certin index of the array.

--- arrays/fill-with-input.cpp
+++ arrays/fill-with-input.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 int main(){
-    
+    string foods[5];
+    int size = sizeof(foods)/sizeof(foods[0]);
+
+    for(int i = 0; i < size; i++){
+        i++;
+        cout << "Enter a food you like #" << i << ": ";
+        i--;
+        getline(cin, foods[i]);
+    }
 
     return 0;
 }
