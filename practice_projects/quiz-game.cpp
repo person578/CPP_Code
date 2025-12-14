bc1c5b565a0acd0b7bdd1ffc9a48fa279a7e60b2
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-14 13:21:02 -0600

    Made an array of questions

--- practice_projects/quiz-game.cpp
+++ practice_projects/quiz-game.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    
-    
+    string questions[] = {"1. What is 5+5?", "2. What is 5^2?", "3. What is the sqrt of 25?", "4. What is 25^2?", "5. What is the sqrt of 625?"};
+
     return 0;
 }
