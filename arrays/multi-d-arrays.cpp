eaf73c3de4843f59dcde8934de4010174097b540
SUPER-SECRET-HACKERMAN <ethanrocks500@proton.me>
2025-12-13 22:24:33 -0600

    Added 2D string array for different cars

--- arrays/multi-d-arrays.cpp
+++ arrays/multi-d-arrays.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 int main(){
-
+    string cars[][3] = {{"Mustang", "Escape", "F-150"},
+                       {"Corvette", "Equinox", "Silverado"},
+                       {"Challenger", "Durango", "Ram 1500"}};
+    
+    
 
     return 0;
 }
