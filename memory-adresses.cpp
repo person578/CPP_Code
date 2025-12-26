f8b66e2d8c5bb72f3832c011666a10924640831e
Codinglynd <ethanrocks500@proton.me>
2025-12-26 00:24:00 -0600

    commit

--- memory-adresses.cpp
+++ memory-adresses.cpp
@@ -7,7 +7,7 @@ int main(){
     bool student = true;
 
 
-    // the '&' symbol is the "address of" operator which shows the address of the thing you put next to it 
+    // the '&' symbol is the "address of" operator which shows the address of the thing you put next to it
     cout << &name << '\n';
     cout << &age << '\n';
     cout << &student << '\n';
